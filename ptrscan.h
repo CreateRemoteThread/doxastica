ef6830b405f437c554f3253eadaad7bbbfee6ad9
CreateRemoteThread <norman.yue@owasp.org>
2016-01-11 19:11:41 +1100

    now it compiles

--- ptrscan.h
+++ ptrscan.h
@@ -1,9 +1,10 @@
 #define PTRSCAN_SIG 0x01440245
 
-static int cs_ptrscan(lua_State *L);
-int validatePtrResult(ptrResult *p);
 
 struct ptrResult
 {
 	DWORD signature;
 };
+
+static int cs_ptrscan(lua_State *L);
+int validatePtrResult(ptrResult *p);
